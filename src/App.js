12f04d7473f9ba0ca74d6672e40afb7a68f4282c
Niharika <124699540+Niharika23612@users.noreply.github.com>
2025-03-09 22:59:12 +0530

    student page
    -niharika

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import StudentPage from "./pages/StudentPage";
 
 function App() {
   const [isLogin, setIsLogin] = useState(true);
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login setIsLogin={setIsLogin} />} />
         <Route path="/signup" element={<Signup setIsLogin={setIsLogin} />} />
+        <Route path="/student" element={<StudentPage />} />
       </Routes>
     </Router>
   );
